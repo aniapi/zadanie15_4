ad94c08732fa7d46f6784b0260e5d5b9da7f4f39
Anna Piwnik <a.piwnik@o2.pl>
2018-03-14 14:44:18 +0100

    add files

---
+++ script.js
@@ -0,0 +1,21 @@
+const hello = "Hello";
+const world = "World";
+
+function sayHello() {
+	console.log(${hello}, ${world});
+}
+sayHello();
+
+
+
+// const sayHello = () => alert('Hello world!');
+// sayHello();
+
+
+
+
+
+
+var app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
+
